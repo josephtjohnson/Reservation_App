7b31228dfd3300089a21a93e976d68f5135bb8db
Joseph Johnson <82980802+josephtjohnson@users.noreply.github.com>
2021-10-22 10:35:59 -0400

    Initial commit

---
+++ src/Reservation.java
@@ -0,0 +1,18 @@
+public class Reservation {
+  private Customer customer;
+  private IRoom room;
+  private Date checkInDate;
+  private Date checkOutDate;
+  
+  public Reservation(Customer customer, IRoom room, Date checkInDate, Date checkOutDate) {
+    this.customer = customer;
+    this.room = room;
+    this.checkInDate = checkInDate;
+    this.checkOutDate = checkOutDate;
+  }
+  
+  @Override
+  public String toString() {
+    return "Reservation: "+ customer + "" + room + "" + "Dates: " + checkInDate + - + checkOutDate;
+  }
+}
