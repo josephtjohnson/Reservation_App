13fa42b4b1f7b8ee82d0cbd3a689857d213016c9
Joseph Johnson <johnson_jt@outlook.com>
2021-10-22 23:39:00 -0400

    Remove import from CustomerService

--- src/service/CustomerService.java
+++ src/service/CustomerService.java
@@ -3,7 +3,6 @@ package service;
 import model.Customer;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.Locale;
 
 public class CustomerService {
     Collection<Customer> customers = new HashSet<Customer>();
