8fa0737b9d3b10a209687cb9d49682ae20f0a241
Joseph Johnson <johnson_jt@outlook.com>
2021-10-27 15:47:03 -0400

    Add methods from Service classes

--- src/api/HotelResource.java
+++ src/api/HotelResource.java
@@ -1,2 +1,30 @@
-package api;public class HotelResource {
+package api;
+
+import model.IRoom;
+import model.Reservation;
+import service.CustomerService;
+import service.ReservationService;
+
+import java.util.Collection;
+import java.util.Date;
+
+public class HotelResource {
+    public CustomerService getCustomer(String email) {
+        
+    }
+    public void createACustomer(String email, String firstName, String lastName) {
+
+    }
+    public IRoom getRoom(String roomNumber){
+
+    }
+    public Reservation bookARoom(String customerEmail, IRoom room, Date checkInDate, Date checkOutDate) {
+
+    }
+    public Collection<Reservation> getCustomerReservations(String customerEmail) {
+
+    }
+    public Collection<IRoom> findARoom(Date checkInDate, Date checkOutDate) {
+
+    }
 }
