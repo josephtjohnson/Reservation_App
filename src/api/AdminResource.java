a3add3aa90c7b61381b650b8b27180f0bf0073ad
Joseph Johnson <johnson_jt@outlook.com>
2021-11-01 16:49:45 -0400

    Update format

--- src/api/AdminResource.java
+++ src/api/AdminResource.java
@@ -1,2 +1,42 @@
-package api;public class AdminResource {
+package api;
+
+import model.Customer;
+import model.IRoom;
+import model.Room;
+import service.CustomerService;
+import service.ReservationService;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
+public class AdminResource {
+    private static AdminResource adminResource = null;
+    public static final Collection<IRoom> rooms = new HashSet<>();
+    public static final CustomerService customerService = CustomerService.getInstance();
+    public static final ReservationService reservationService = ReservationService.getInstance();
+
+    public static AdminResource getInstance() {
+        if(null == adminResource){
+            AdminResource adminResource = new AdminResource();
+        }
+        return adminResource;
+    }
+    public Customer getCustomer(String email){
+        return customerService.getCustomer(email);
+    }
+    public void addRoom(List<IRoom> rooms){
+        for (IRoom room : rooms) {
+            reservationService.addRoom(room);
+        }
+    }
+    public Collection<IRoom> getAllRooms(){
+        return reservationService.getRooms();
+    }
+    public Collection<Customer> getAllCustomers(){
+        return customerService.getCustomers();
+    }
+    public void displayAllReservations(){
+        reservationService.printAllReservations();
+    }
 }
