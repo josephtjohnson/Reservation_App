4b3dd20b2db7ff6743ae1fa7132b8228e41bfd29
Joseph Johnson <82980802+josephtjohnson@users.noreply.github.com>
2021-11-17 10:01:58 -0500

    Add and override hashcode() and equals()

--- src/model/Reservation.java
+++ src/model/Reservation.java
@@ -34,4 +34,30 @@ public class Reservation extends ArrayList{
   public String toString() {
     return customer.getFullName() + " -- " + room + " " + "Dates: " + checkInDate + " - " + checkOutDate;
   }
+  
+  @Override
+  public int hashCode() {
+    int hash = 7;
+    hash =  31 * hash + Objects.hashCode(this.customer) + Objects.hashCode(this.room) + Objects.hashCode(this.checkInDate) + Objects.hashCode(this.checkOutDate) + Objects.hashCode(this.isFree)
+    return hash;
+  }
+  
+  @Override
+  public boolean equals (Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (obj instanceof Reservation) {
+      Reservation test = (Reservation) obj;
+      if (test.customer == this.customer && test.room == this.room && test.checkInDate == this.checkInDate 
+          && test.checkOutDate == this.checkOutDate && test.isFree == this.isFree) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
 }
